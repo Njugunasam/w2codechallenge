4042a4d6fa08a35a309b5b31d13f36ff37ea6872
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-17 19:42:08 +0300

    implemented

--- index.js
+++ index.js
@@ -1,67 +1,39 @@
-// Define initial animal details
-const initialAnimalList = [
-    {
-      "id": 1,
-      "name": "Mr. Cute",
-      "image": "https://bestanimations.com/gifs/cute-bird-act-without-expectation.html",
-      "votes": 0
-    },
-    {
-      "id": 2,
-      "name": "Mx. Monkey",
-      "image": "https://bestanimations.com/gifs/Pretty-Monkey.html",
-      "votes": 0
-    },
-    {
-      "id": 3,
-      "name": "Ms. Zebra",
-      "image": "https://bestanimations.com/gifs/zebra-running.html",
-      "votes": 0
-    },
-    {
-      "id": 4,
-      "name": "Dr. Lion",
-      "image": "https://bestanimations.com/gifs/Lion-Running-In-Jungle.html",
-      "votes": 0
-    },
-    {
-      "id": 5,
-      "name": "Mme. Panda",
-      "image": "https://bestanimations.com/gifs/Cute-Panda-Bear-Cartoon.html",
-      "votes": 0
-    }
-  ];
-  
-  // Function to display all animal details
-  function showAllAnimalDetails() {
-    const animalList = document.getElementById('animal-list');
-  
-    initialAnimalList.forEach(animal => {
-      const animalDiv = document.createElement('div');
-      animalDiv.className = 'animal-details';
-  
-      const animalImage = document.createElement('img');
-      animalImage.src = animal.image;
-      animalImage.alt = animal.name;
-  
-      const voteCount = document.createElement('p');
-      voteCount.textContent = `Votes: ${animal.votes}`;
-  
-      const voteButton = document.createElement('button');
-      voteButton.textContent = 'Vote';
-      voteButton.addEventListener('click', () => {
-        animal.votes++;
-        voteCount.textContent = `Votes: ${animal.votes}`;
-      });
-  
-      animalDiv.appendChild(animalImage);
-      animalDiv.appendChild(voteCount);
-      animalDiv.appendChild(voteButton);
-      animalList.appendChild(animalDiv);
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('http://localhost:3000/initialAnimalList')
+    .then(response => response.json())
+    .then(data => {
+      showAllAnimalList(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
     });
-  }
-  
-  // Fetch animal details
-  document.addEventListener("DOMContentLoaded", function () {
-    showAllAnimalDetails();
+});
+
+function showAllAnimalList(data) {
+  const animalList = document.getElementById('animal-list');
+
+  data.forEach(animal => {
+    const animalDiv = document.createElement('div');
+    animalDiv.className = 'animal-details';
+
+    const animalImage = document.createElement('img');
+animalImage.src = animal.image;
+animalImage.alt = animal.name;
+animalImage.className = 'animal-image'; 
+
+    const voteCount = document.createElement('p');
+    voteCount.textContent = `Votes: ${animal.votes}`;
+
+    const voteButton = document.createElement('button');
+    voteButton.textContent = 'Vote';
+    voteButton.addEventListener('click', () => {
+      animal.votes++;
+      voteCount.textContent = `Votes: ${animal.votes}`;
+    });
+
+    animalDiv.appendChild(animalImage);
+    animalDiv.appendChild(voteCount);
+    animalDiv.appendChild(voteButton);
+    animalList.appendChild(animalDiv);
   });
+}
