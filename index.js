b105624508b4f383047527e2e3513f308744318d
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-19 18:57:11 +0300

    implemented on js

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("  http://localhost:3000/characters")
-    .then(response => response.json())
-    .then(data => {
-      showAllcharacters(data);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+//fetching data from db
+document.addEventListener("DOMContentLoaded", function () { 
+  fetch(" http://localhost:3000/characters") 
+    .then(response => response.json()) 
+    .then(data => { 
+      showAllCharacters(data); 
+    }) 
+    .catch(error => { 
+      console.error('Error fetching data:', error); 
+    }); 
 });
-//difine show all characters
-function showAllcharacters(data) {
-  const animalList = document.getElementById('animal-list');
+//define showallcharacters
+function showAllCharacters(data) {
+  const animals_namesContainer = document.getElementById('animal-names');
+  const Animal_ImagesContainer = document.getElementById('animal-image');
+  const voteButton = document.getElementById('vote-button');
+  const voteCount = document.getElementById('vote-count');
+  let currentAnimal = null;
+
+  const namesRow = document.createElement('div'); 
+  namesRow.style.display = 'flex';
 
   data.forEach(animal => {
-    const animalDiv = document.createElement('div');
-    animalDiv.className = 'animal-details';
+    const animalName = document.createElement('p');
+    animalName.textContent = animal.name;
+    animalName.style.margin = '0 10px';
 
-    const animalImage = document.createElement('img');
-animalImage.src = animal.image;
-animalImage.alt = animal.name;
-animalImage.className = 'animal-image'; 
-
-    const voteCount = document.createElement('p');
-    voteCount.textContent = `Votes: ${animal.votes}`;
-
-    const voteButton = document.createElement('button');
-    voteButton.textContent = 'Vote';
-    voteButton.addEventListener('click', () => {
-      animal.votes++;
-      voteCount.textContent = `Votes: ${animal.votes}`;
+    animalName.addEventListener('click', () => {
+      if (currentAnimal === animal) {
+        Animal_ImagesContainer.innerHTML = '';
+        currentAnimal = null;
+      } else {
+        const Animal_Images = document.createElement('img');
+        Animal_Images.src = animal.image;
+        Animal_Images.alt = animal.name;
+        Animal_ImagesContainer.innerHTML = '';
+        Animal_ImagesContainer.appendChild(Animal_Images);
+        currentAnimal = animal;
+        voteCount.textContent = `Votes: ${animal.votes}`;
+      }
     });
 
-    animalDiv.appendChild(animalImage);
-    animalDiv.appendChild(voteCount);
-    animalDiv.appendChild(voteButton);
-    animalList.appendChild(animalDiv);
+    namesRow.appendChild(animalName);
+  });
+
+  animals_namesContainer.appendChild(namesRow);
+
+  voteButton.addEventListener('click', () => {
+    if (currentAnimal) {
+      currentAnimal.votes++;
+      voteCount.textContent = `Votes: ${currentAnimal.votes}`;
+    }
   });
 }
