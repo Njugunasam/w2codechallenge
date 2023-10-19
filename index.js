eb809dd6e4a5dbfb1824b7f4e2c670c0fd7d8c10
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-19 18:58:13 +0300

    implemented on js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }) 
     .catch(error => { 
       console.error('Error fetching data:', error); 
-    }); 
+    });  
 });
 //define showallcharacters
 function showAllCharacters(data) {
